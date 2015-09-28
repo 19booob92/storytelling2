da30059ccaa3acfe3be1d1dbb4a5454ab3723d92
Tomasz Kaczmarzyk <tomasz+github@kaczmarzyk.net>
2015-09-28 15:43:19 +0200

    DRY refactoring -> cumbersome test setup

--- src/test/java/net/kaczmarzyk/DocumentTest.java
+++ src/test/java/net/kaczmarzyk/DocumentTest.java
@@ -21,6 +21,22 @@ public class DocumentTest {
     Person bart = new Person("Bart Simpson");
     Person peter = new Person("Peter Griffin");
 
+    Document submitedDocumentAuthoredByHomerWithEditorBart = document()
+            .withStatus(SUBMITED)
+            .authoredBy(homer)
+            .withEditor(bart)
+            .build();
+    
+    Document rejectedDocumentAuthoredByHomer = document()
+            .authoredBy(homer)
+            .withStatus(REJECTED)
+            .build();
+
+    Document draftDocumentAuthoredByHomerWithEditorBart = document()
+            .authoredBy(homer)
+            .withEditor(bart)
+            .build();
+    
     @Test
     public void newDocumentHasStatusDraft() {
         Document document = document().build();
@@ -29,136 +45,78 @@ public class DocumentTest {
 
     @Test
     public void authorCanAmendContentOfADraft() {
-        Document document = document()
-                .authoredBy(homer)
-                .build();
+        draftDocumentAuthoredByHomerWithEditorBart.amend("new content", homer);
 
-        document.amend("new content", homer);
-
-        assertThat(document).hasContent("new content");
+        assertThat(draftDocumentAuthoredByHomerWithEditorBart).hasContent("new content");
     }
 
     @Test
     public void authorCanAmendTheContentOfADraftWithoutCreatingNewRevision() {
-        Document document = document()
-                .authoredBy(homer)
-                .build();
+        draftDocumentAuthoredByHomerWithEditorBart.amend("new content", homer);
 
-        document.amend("new content", homer);
-
-        assertThat(document).hasRevisionNumber(1);
+        assertThat(draftDocumentAuthoredByHomerWithEditorBart).hasRevisionNumber(1);
     }
 
     @Test
     public void documentCanBeAmendedOnlyByAuthor() {
-        Document document = document()
-                .authoredBy(homer)
-                .build();
-
         exception.expect(IllegalArgumentException.class);
 
-        document.amend("new content", bart);
+        submitedDocumentAuthoredByHomerWithEditorBart.amend("new content", bart);
     }
 
     @Test
     public void newRevisionIsCreatedWhenAuthorEditsARejectedDocument() {
-        Document document = document()
-                .authoredBy(homer)
-                .withStatus(REJECTED)
-                .build();
+        rejectedDocumentAuthoredByHomer.amend("new content", homer);
 
-        document.amend("new content", homer);
-
-        assertThat(document).hasRevisionNumber(2);
+        assertThat(rejectedDocumentAuthoredByHomer).hasRevisionNumber(2);
     }
 
     @Test
     public void authorCanAmendContentOfARejectedDocument() {
-        Document document = document()
-                .authoredBy(homer)
-                .withStatus(REJECTED)
-                .build();
+        rejectedDocumentAuthoredByHomer.amend("new content", homer);
 
-        document.amend("new content", homer);
-
-        assertThat(document).hasContent("new content");
+        assertThat(rejectedDocumentAuthoredByHomer).hasContent("new content");
     }
     
     @Test
     public void editorMayRejectSubmitedDocument() {
-        Document document = document()
-                .withStatus(SUBMITED)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
+        submitedDocumentAuthoredByHomerWithEditorBart.reject(bart);
         
-        document.reject(bart);
-        
-        assertThat(document).hasStatus(REJECTED);
+        assertThat(submitedDocumentAuthoredByHomerWithEditorBart).hasStatus(REJECTED);
     }
     
     @Test
     public void editorCantRejectDocumentThatHasNotBeenSubmited() {
-        Document document = document()
-                .withStatus(DRAFT)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
-        
         exception.expect(IllegalStateException.class);
         
-        document.reject(bart);
+        draftDocumentAuthoredByHomerWithEditorBart.reject(bart);
     }
     
     @Test
     public void onlyEditorsMayRejectDocument() {
-        Document document = document()
-                .withStatus(SUBMITED)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
-        
         exception.expect(IllegalArgumentException.class);
         
-        document.reject(peter);
+        submitedDocumentAuthoredByHomerWithEditorBart.reject(peter);
     }
     
     @Test
     public void editorMayAcceptSubmitedDocument() {
-        Document document = document()
-                .withStatus(SUBMITED)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
+        submitedDocumentAuthoredByHomerWithEditorBart.accept(bart);
         
-        document.accept(bart);
-        
-        assertThat(document).hasStatus(ACCEPTED);
+        assertThat(submitedDocumentAuthoredByHomerWithEditorBart).hasStatus(ACCEPTED);
     }
     
     @Test
     public void onlyEditorMayAcceptDocument() {
-        Document document = document()
-                .withStatus(SUBMITED)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
-        
         exception.expect(IllegalArgumentException.class);
         
-        document.accept(peter);
+        submitedDocumentAuthoredByHomerWithEditorBart.accept(peter);
     }
-    
+
     @Test
     public void editorCantAcceptDocumentThatHasNotBeenSubmited() {
-        Document document = document()
-                .withStatus(DRAFT)
-                .authoredBy(homer)
-                .withEditor(bart)
-                .build();
-        
         exception.expect(IllegalStateException.class);
         
-        document.accept(bart);
+        draftDocumentAuthoredByHomerWithEditorBart.accept(bart);
     }
 }
